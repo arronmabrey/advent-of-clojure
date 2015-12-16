9bff9ad734490bedb920f526823bff272ae67520
Arron Mabrey <arron@mabreys.com>
2015-12-16 10:22:18 -0500

    refactor day01

--- src/advent/day01.clj
+++ src/advent/day01.clj
@@ -4,6 +4,16 @@
 
 (def prob (str/trim (slurp (io/resource "prob1"))))
 
-(def part1 (reduce + (map #(if (= % \() 1 -1) prob)))
+(def part1
+  (->>  prob
+        (map #(if (= % \() 1 -1))
+        (reduce +)))
 
-(def part2 (reduce (fn [acc, [idx, step]] (let [r (+ acc step)] (if (= r -1) (reduced (inc idx)) r))) 0 (map-indexed #(vec [%1 (if (= %2 \() 1 -1)]) prob)))
+(def part2
+  (->>  prob
+        (map-indexed #(vec [%1 (if (= %2 \() 1 -1)]))
+        (reduce (fn [acc [idx step]]
+                  (if (= (+ acc step) -1)
+                      (reduced (inc idx))
+                      (+ acc step)))
+                0)))
