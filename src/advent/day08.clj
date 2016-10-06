464ae50953eb93e566d90b7ed06996d3795971b4
Arron Mabrey <arron@mabreys.com>
2016-10-06 02:47:49 -0400

    add advent.day08

---
+++ src/advent/day08.clj
@@ -0,0 +1,26 @@
+(ns advent.day08
+  (:require [clojure.tools.namespace.repl :refer [refresh]]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def prob (->> "prob8" (io/resource) (io/reader) line-seq))
+
+(defn unescape-count [line]
+  (->> line
+       (re-seq #"[a-zA-Z0-9]|\\\"|\\x[0-9a-f]{2}|\\\\")
+       (count)))
+
+(defn escape-count [line]
+  (-> line
+      (str/replace "\\" "\\\\")
+      (str/replace "\"" "\\\"")
+      (count)
+      (+ 2)))
+
+(defn map-sum [f1 f2 c]
+  (reduce + (map #(- (f1 %) (f2 %)) c)))
+
+(map-sum count unescape-count prob) ;;=> 1342
+(map-sum escape-count count prob) ;;=> 2074
+
+;;
