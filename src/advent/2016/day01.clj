71936d0c9afa5b3d367f9d1aad97965a521b09ee
Arron Mabrey <arron@mabreys.com>
2017-01-12 17:02:23 -0500

    update 2016 - day01

--- src/advent/2016/day01.clj
+++ src/advent/2016/day01.clj
@@ -8,28 +8,34 @@
 (def samp2 '("R2, R2, R2"))
 (def samp3 '("R5, L5, R5, R3"))
 (def samp4 '("R5, L525, R5, R3"))
+(def samp5 '("R8, R4, R4, R8"))
 
-(def parse-line
-  (comp read-string #(str \[ % \])))
+(def rotation-map {"R" {"N" "E" "E" "S" "S" "W" "W" "N"} "L" {"N" "W" "W" "S" "S" "E" "E" "N"}})
+(def coordinate-map {"N" [0 1] "E" [1 0] "S" [0 -1] "W" [-1 0]})
+(def init-state {:heading "N" :coordinate [0 0]})
+(def parse-line (comp read-string #(str \[ % \])))
+(def split-rotation-qty (comp (partial drop 1) #(re-find #"([RL])(\d+)" %) str))
 
-(def rk {"R" {"N" "E" "E" "S" "S" "W" "W" "N"} "L" {"N" "W" "W" "S" "S" "E" "E" "N"}})
-(def pk {"N" [0 1] "E" [1 0] "S" [0 -1] "W" [-1 0]})
+(defn next-coordinate [next-heading current-coordinate qty]
+  (reduce (fn [[current-x current-y] [next-x next-y]]
+            [(+ current-x next-x) (+ current-y next-y)])
+          current-coordinate
+          (repeat (read-string qty) (get coordinate-map next-heading))))
 
 (->> prob
      (mapcat parse-line)
-     (map (comp (partial drop 1) #(re-find #"([RL])(\d+)" %) str))
-     (reduce (fn [acc [r n]]
-               (let [d (get-in rk [r (:d acc)])
-                     p (:p acc)
-                     ps (repeat (read-string n) (get pk d))]
-                 (-> acc
-                     (assoc :d d)
-                     (assoc :p (reduce (fn [[a1 a2] [d1 d2]] [(+ a1 d1) (+ a2 d2)])
-                                       p
-                                       ps)))))
-             {:d "N" :p [0 0]})
-     (:p)
-     (apply +)
-     (Math/abs))
+     (map split-rotation-qty)
+     (reduce (fn [state [rotation qty]]
+               (let [current-heading (:heading state)
+                     current-coordinate (:coordinate state)
+                     next-heading (get-in rotation-map [rotation current-heading])
+                     next-coordinate (next-coordinate next-heading current-coordinate qty)]
+                 {:heading next-heading :coordinate next-coordinate}))
+             init-state)
+     (:coordinate)
+     (map #(Math/abs %))
+     (apply +))
+
+
 
 ;;
