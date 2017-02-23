228ebbbc64ba507f011c763fe9a1d816e5f800c5
Arron Mabrey <arron@mabreys.com>
2017-02-23 09:03:53 -0500

    refactor 2015/day1

--- src/advent/2015/day01.clj
+++ src/advent/2015/day01.clj
@@ -2,18 +2,19 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def prob (str/trim (slurp (io/resource "2015/prob1"))))
+(def prob (-> "2015/prob1" io/resource slurp str/trim))
+
+(def lookup {\( 1 \) -1})
 
 (defn part1 []
   (->>  prob
-        (map #(if (= % \() 1 -1))
+        (map lookup)
         (reduce +)))
 
 (defn part2 []
   (->>  prob
-        (map-indexed #(vec [%1 (if (= %2 \() 1 -1)]))
-        (reduce (fn [acc [idx step]]
-                  (if (= (+ acc step) -1)
-                    (reduced (inc idx))
-                    (+ acc step)))
-                0)))
+        (map lookup)
+        (reductions +)
+        (take-while #(not= -1 %))
+        count
+        inc))
